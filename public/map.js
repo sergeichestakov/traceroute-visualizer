6bd9055dcd27607849d9ed44cc3676ae038401ac
Sergei Chestakov <schestakov@ucdavis.edu>
2018-05-28 13:47:29 -0700

    Serve html file with map

---
+++ public/map.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2VyZ2VpY2hlc3Rha292IiwiYSI6ImNqNGs2NzdxMzBnMWYyd3FqOWlxd2N1ZWkifQ.cVaGTeATJmDDq6ULte67MQ';
+
+const map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-121.74, 38.54], // starting position
+    zoom: 6 // starting zoom
+});
+
+// Add geolocate control to the map.
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
