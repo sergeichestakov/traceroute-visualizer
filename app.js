b2c08b96ba06fbeaf0438ab982265f5db1be84cd
Sergei Chestakov <schestakov@ucdavis.edu>
2018-05-28 16:10:58 -0700

    Call traceroute and extract IPs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const traceroute = require('traceroute');
 
 app.use(bodyParser.json()) // to support JSON-encoded bodies
 app.use(express.json())
@@ -13,6 +14,19 @@ app.get('/', (req, res) =>
 app.post('/request', (req, res) => {
 	const value = req.body.value
     console.log(value)
+	traceroute.trace(value, (err,hops) => {
+		if (err) { //Handle error
+			console.log(err)
+		}
+		//Iterate through return JSON and extract IPs
+		for (const hopKey in hops) {
+			const hop = hops[hopKey]
+			for (const IP in hop) {
+				console.log('IP:', IP)
+			}
+		}
+
+	});
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
